98f9c1391cd0795bad9322a8384cd53da9b0ab63
M11-cell <michael.lythgoe@spaceconcordia.ca>
2025-08-11 14:55:21 -0400

    modified ik files now fully functioning on Rviz

---
+++ launch_files/moveit_launch.py
@@ -0,0 +1,143 @@
+import os
+from launch import LaunchDescription
+from launch_param_builder import ParameterBuilder
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+
+def generate_launch_description():
+    moveit_config = (
+        MoveItConfigsBuilder("ceres")
+        .robot_description(file_path="config/ceres_rover.urdf.xacro")
+        .joint_limits(file_path="config/joint_limits.yaml")
+        .to_moveit_configs()
+    )
+
+    servo_params = {
+        "moveit_servo": ParameterBuilder("ceres_moveit_config")
+        .yaml("config/ceres_servo.yaml")
+        .to_dict()
+    }
+
+    rviz_config_file = (
+        get_package_share_directory("moveit_servo")
+        + "config/moveit.rviz"
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+        ],
+    )
+
+    # ros2_control using FakeSystem as hardware
+    ros2_controllers_path = os.path.join(
+        get_package_share_directory("ceres_moveit_config"),
+        "config",
+        "ros2_controllers.yaml",
+    )
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[moveit_config.robot_description, ros2_controllers_path],
+        output="screen",
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager-timeout",
+            "300",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    ceres_arm_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["ceres_arm_controller", "-c", "/controller_manager"],
+    )
+
+        # Launch as much as possible in components
+    container = ComposableNodeContainer(
+        name="moveit_servo_demo_container",
+        namespace="/",
+        package="rclcpp_components",
+        executable="component_container_mt",
+        composable_node_descriptions=[
+            # Example of launching Servo as a node component
+            # Assuming ROS2 intraprocess communications works well, this is a more efficient way.
+            # ComposableNode(
+            #     package="moveit_servo",
+            #     plugin="moveit_servo::ServoServer",
+            #     name="servo_server",
+            #     parameters=[
+            #         servo_params,
+            #         moveit_config.robot_description,
+            #         moveit_config.robot_description_semantic,
+            #     ],
+            # ),
+            ComposableNode(
+                package="robot_state_publisher",
+                plugin="robot_state_publisher::RobotStatePublisher",
+                name="robot_state_publisher",
+                parameters=[moveit_config.robot_description],
+            ),
+            ComposableNode(
+                package="tf2_ros",
+                plugin="tf2_ros::StaticTransformBroadcasterNode",
+                name="static_tf2_broadcaster",
+                parameters=[{"child_frame_id": "/base_structure_link", "frame_id": "/world"}],
+            ),
+            ComposableNode(
+                package="joy",
+                plugin="joy::Joy",
+                name="joy_node",
+            ),
+        ],
+        output="screen",
+    )
+        # Launch a standalone Servo node.
+    # As opposed to a node component, this may be necessary (for example) if Servo is running on a different PC
+
+    ik_mux_node = Node(
+        package="ik_mux_controller",
+        executable="ik_mux",
+        name="ik_mux",
+        output="screen",
+    )
+    servo_node = Node(
+        package="moveit_servo",
+        executable="servo_node_main",
+        parameters=[
+            servo_params,
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+        ],
+        output="screen",
+    )
+
+    return LaunchDescription(
+        [
+            rviz_node,
+            ros2_control_node,
+            joint_state_broadcaster_spawner,
+            ceres_arm_controller_spawner,
+            ik_mux_node,
+            servo_node,
+            container,
+        ]
+    )
