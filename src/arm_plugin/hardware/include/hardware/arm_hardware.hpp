23a05142c8b15c1c531e80a741ea31967d5e777f
M11-cell <michael.lythgoe@spaceconcordia.ca>
2025-08-28 14:58:25 -0400

    hardware interface setup for arm

---
+++ src/arm_plugin/hardware/include/hardware/arm_hardware.hpp
@@ -0,0 +1,50 @@
+// Copyright 2023 ros2_control Development Team
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef arm_hardware__R6BOT_HARDWARE_HPP_
+#define arm_hardware__R6BOT_HARDWARE_HPP_
+
+#include "string"
+#include "unordered_map"
+#include "vector"
+
+#include "hardware_interface/handle.hpp"
+#include "hardware_interface/hardware_info.hpp"
+#include "hardware_interface/system_interface.hpp"
+#include "hardware_interface/types/hardware_interface_return_values.hpp"
+#include "hardware_interface/types/hardware_interface_type_values.hpp"
+
+using hardware_interface::return_type;
+
+namespace arm_hardware
+{
+using CallbackReturn = rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn;
+
+class RobotSystem : public hardware_interface::SystemInterface
+{
+public:
+  CallbackReturn on_init(const hardware_interface::HardwareInfo & info) override;
+
+  CallbackReturn on_configure(const rclcpp_lifecycle::State & previous_state) override;
+
+  return_type read(const rclcpp::Time & time, const rclcpp::Duration & period) override;
+
+  return_type write(const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/) override;
+
+protected:
+};
+
+}  // namespace arm_hardware
+
+#endif  // arm_hardware__R6BOT_HARDWARE_HPP_
