c7c4207f15e01c5cc3d4799ee649ff28b1442f9b
M11-cell <michael.lythgoe@spaceconcordia.ca>
2025-08-29 15:24:06 -0400

    modified hardware interface

--- src/arm_plugin/hardware/rover_hardware.cpp
+++ src/arm_plugin/hardware/rover_hardware.cpp
@@ -1,4 +1,4 @@
-#include "arm_hardware/r6bot_hardware.hpp"
+#include "hardware/arm_hardware.hpp"
 #include <string>
 #include <vector>
 
@@ -6,41 +6,37 @@
 
 namespace arm_hardware
 {
- CallbackReturn on_init(const hardware_interface::HardwareInfo &info)
+ CallbackReturn RobotSystem::on_init(const hardware_interface::HardwareInfo &info)
  {
-    if (hardware_interface::SystemInterface::on_init(info) != CallbackRetrun::SUCCESS)
+    if (SystemInterface::on_init(info) != CallbackReturn::SUCCESS)
     {
         return CallbackReturn::ERROR;
     }
     //Setup communication with robot hardware
     return CallbackReturn::SUCCESS;
  }
-
-}
-CallbackReturn RobotSystem::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
+ CallbackReturn RobotSystem::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
+ {
     // Setup communication with robot hardware
-{
     return CallbackReturn::SUCCESS;
-}
-CallbackReturn RobotSystem::read(const rclcpp::Time & time, const rclcpp::Duration &period)
-{
+ }
+ return_type RobotSystem::read(const rclcpp::Time & time, const rclcpp::Duration &period)
+ {
     //read hardware values for the state interfaces, ex: joint encoders and sensor readings. 
 
     return return_type::OK;
-}
-return_type::write(const rclcpp::Time & time, const rclcpp::Duration & period)
-{
+ }
+ return_type RobotSystem::write(const rclcpp::Time & time, const rclcpp::Duration & period)
+ {
     //send commands interface values to hardware, e.g joint set joint velocity.
 
     return return_type::OK;
+ }
+
 }
-
-
 #include "pluginlib/class_list_macros.hpp"
 
-PLUGIN_EXPORT_CLASS(arm_hardware::RobotSystem, hardware_interface::SystemInterface)
-
-
+PLUGINLIB_EXPORT_CLASS(arm_hardware::RobotSystem, hardware_interface::SystemInterface)
 
 
 
