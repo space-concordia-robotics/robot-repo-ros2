3bed65e5fd71f7f00b1493f77ed97a4484ad10af
M11-cell <michael.lythgoe@spaceconcordia.ca>
2025-12-20 21:52:08 -0500

    modified hw interface

--- launch_files/arm_hardware.launch.py
+++ launch_files/arm_hardware.launch.py
@@ -8,45 +8,37 @@ from launch.event_handlers import OnProcessStart
 from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description(): 
     
-    package_name="ceres_moveit_config"
+    urdf_path = os.path.join(get_package_share_directory('rover_arm_description'), 'urdf', 'arm.urdf.xacro')
 
-     # Declare the HW_mode launch argument
-    declare_hw_mode_arg = DeclareLaunchArgument(
-        "HW_mode",
-        default_value="true",
-        description="Flag to set hardware vs. simulation mode."
-    )
-
-    HW_mode = LaunchConfiguration("HW_mode")
-
-    #launching robot_state_publisher 
-    rsp = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')]),
-        launch_arguments={"HW_mode" : HW_mode}.items()
-    )
+    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
 
     controller_config = os.path.join(
-        get_package_share_directory(package_name), 'config', 'ros2_controllers.yaml'
+        get_package_share_directory('rover_arm_bringup'), 'config', 'ros2_controllers.yaml'
     )
+   
 
-    #running the ros2_control node to handle controller spawining and loading 
-    #remap topic to /robot_description
+    rsp_launch = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        parameters=[{'robot_description': robot_description}]
+    )
 
     controller_manager = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[controller_config],
-        remappings=[("/controller_manager/robot_description", "robot_description")],
+        parameters=[controller_config, {'robot_description': robot_description}],
         output="screen",
     )
 
     #delaying controller manager to give time for everything to initialize
     delayed_controller_manager = TimerAction(
         period = 3.0,
-        actions={controller_manager},
+        actions=[controller_manager],
     )
 
 
@@ -64,7 +56,7 @@ def generate_launch_description():
     arm_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["ceres_arm_controller"], # <-- Fixed controller name
+        arguments=["arm_controller"], # <-- Fixed controller name
         output="screen",
     )
 
@@ -87,8 +79,7 @@ def generate_launch_description():
 
     return LaunchDescription([
         
-        declare_hw_mode_arg,
-        rsp,
+        rsp_launch,
         delayed_controller_manager,
         delayed_spawners,
         # ik_mux_node,  # Temporarily commented out
