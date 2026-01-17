9e744b0d5807f85df2609934459a419c7b15dd1e
M11-cell <michael.lythgoe@spaceconcordia.ca>
2026-01-17 11:52:04 -0500

    modified ik_mux controller code

--- src/inverse-kinematics/ik_mux_controller/src/ik_mux.cpp
+++ src/inverse-kinematics/ik_mux_controller/src/ik_mux.cpp
@@ -108,17 +108,6 @@ bool convertJoyToCmd(const std::vector<float>& axes, const std::vector<int>& but
   return true;
 }
 
-/** \brief // This should update the frame_to_publish_ as needed for changing command frame via controller
- * @param frame_name Set the command frame to this
- * @param buttons The vector of discrete controller button values
- */
-void updateCmdFrame(std::string& frame_name, const std::vector<int>& buttons)
-{
-  if (buttons[CHANGE_VIEW] && frame_name == EEF_FRAME_ID)
-    frame_name = BASE_FRAME_ID;
-  else if (buttons[SHARE] && frame_name == BASE_FRAME_ID)
-    frame_name = EEF_FRAME_ID;
-}
 
 namespace moveit_servo
 {
@@ -157,29 +146,6 @@ public:
     // Create the messages we might publish
     auto twist_msg = std::make_unique<geometry_msgs::msg::TwistStamped>();
     auto joint_msg = std::make_unique<control_msgs::msg::JointJog>();
-    static bool last_deadman_state = false;
-    bool current_deadman_state = msg->buttons[HOME];
-
-    if(current_deadman_state && !last_deadman_state)
-    {
-      rover_mode = !rover_mode;
-      RCLCPP_INFO(this->get_logger(), "Rover mode switched to %s", rover_mode? "Rover" : "Arm");
-    }
-    last_deadman_state = current_deadman_state;
-
-    if(rover_mode)
-    {
-      //publish the Twist commands for the wheel speeds
-      auto twist_msgs = geometry_msgs::msg::Twist();
-      twist_msgs.linear.x = msg->axes[LEFT_STICK_X];
-      twist_msgs.linear.y = msg->axes[LEFT_STICK_Y];
-      twist_msgs.angular.z = msg->axes[RIGHT_STICK_Y];
-      twist_msgs.linear.z = msg->axes[RIGHT_STICK_X];
-      wheel_pub_->publish(twist_msgs);
-    }
-    else{
-      // This call updates the frame for twist commands
-      updateCmdFrame(frame_to_publish_, msg->buttons);
 
       // Convert the joystick message to Twist or JointJog and publish
       if (convertJoyToCmd(msg->axes, msg->buttons, twist_msg, joint_msg))
@@ -197,7 +163,6 @@ public:
         joint_msg->duration = 1.0;
         joint_pub_->publish(std::move(joint_msg));
       }
-    }
     
   }
 
