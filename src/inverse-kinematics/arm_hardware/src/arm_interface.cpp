1ed84899527993e18773a6c055170594a93e9f43
M11-cell <michael.lythgoe@spaceconcordia.ca>
2026-02-11 13:26:28 -0500

    stuff

--- src/inverse-kinematics/arm_hardware/src/arm_interface.cpp
+++ src/inverse-kinematics/arm_hardware/src/arm_interface.cpp
@@ -409,9 +409,9 @@ namespace arm_interface
         }
 
         // Adding a motor id to control da gripper
-        int gripper = 5; 
-        float gripper_speed = static_cast<float>(gripper)*MAX_MOTOR_SPEED; 
-        memcpy(&out_buf[(gripper*sizeof(float) + 2)], &gripper_speed, sizeof(float));
+        // int gripper = 5; 
+        // float gripper_speed = static_cast<float>(gripper)*MAX_MOTOR_SPEED; 
+        // memcpy(&out_buf[(gripper*sizeof(float) + 2)], &gripper_speed, sizeof(float));
 
         //Firmware might be rejecting previous 19 bites sent to the motors since it might've been expecting a total of 27 bites, hence it
         //would cause the arm not to move. 
