ec8589eed22b3d078007a2d2ef6cf636a6cea6f9
Will <william.free@12oclockpoint.com>
2026-04-30 21:14:13 -0400

    Improve URDF structure
    Restructures the URDF so that it's cleaner & more modular
    
    Signed-off-by: Will <william.free@12oclockpoint.com>

--- rover-description/launch/display.launch.py
+++ rover-description/launch/display.launch.py
@@ -1,8 +1,3 @@
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
-
 from launch_util import SimpleLauncher
 
 
@@ -15,55 +10,21 @@ def generate_launch_description():
 
     sl.include(package="rover_description", launch_file="rsp.launch.py")
 
-    # rviz_config = sl.declare_arg(
-    #     name="rviz_config",
-    #     default_value=sl.find("rover_description", "rviz", "urdf.rviz"),
-    #     description="Absolute path to rviz config file"
-    # )
-    #
-    # sl.declare_arg()
+    rviz_config = sl.declare_arg(
+        name="rviz_config",
+        default_value=sl.find("rover_description", "rviz", "urdf.rviz"),
+        description="Absolute path to rviz config file"
+    )
+
+    sl.include(
+        package='urdf_launch',
+        launch_file='display.launch.py',
+        launch_arguments={
+            "urdf_package": "rover_description",
+            "urdf_package_path": sl.find(package="rover_description", directory="urdf", file="robot.urdf"),
+            "rviz_config": rviz_config,
+            "jsp_gui": "false",
+        }.items()
+    )
 
     return sl.launch_description()
-
-    ld = LaunchDescription()
-
-    rover_description = FindPackageShare("rover_description")
-
-    ld.add_action(
-        DeclareLaunchArgument(
-            name="gui",
-            default_value="true",
-            choices=["true", "false"],
-            description="Flag to enable joint_state_publisher_gui"
-        )
-    )
-
-    ld.add_action(
-        DeclareLaunchArgument(
-            name="rviz_config",
-            default_value=PathJoinSubstitution([rover_description, "rviz", "urdf.rviz"]),
-            description="Absolute path to rviz config file"
-        )
-    )
-
-    ld.add_action(
-        DeclareLaunchArgument(
-            name="model",
-            default_value=PathJoinSubstitution(["urdf", "robot.urdf"]),
-            description="Path to robot urdf file relative to urdf_tutorial package"
-        ),
-    )
-
-    ld.add_action(
-        IncludeLaunchDescription(
-            PathJoinSubstitution([FindPackageShare("urdf_launch"), "launch", "display.launch.py"]),
-            launch_arguments={
-                "urdf_package": "rover_description",
-                "urdf_package_path": LaunchConfiguration("model"),
-                "rviz_config": LaunchConfiguration("rviz_config"),
-                "jsp_gui": LaunchConfiguration("gui"),
-            }.items()
-        )
-    )
-
-    return ld
