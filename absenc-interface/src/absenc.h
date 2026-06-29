b2224255e413288fe8b9fb513cb34c3535194448
Will <william.free@12oclockpoint.com>
2026-06-28 23:25:15 -0400

    Cleanup absenc interface code
    Signed-off-by: Will <william.free@12oclockpoint.com>

--- absenc-interface/src/absenc.h
+++ absenc-interface/src/absenc.h
@@ -1,56 +1,49 @@
 #ifndef ABSENC_H
 #define ABSENC_H
 
-#include <stdio.h>
-#include <string.h>
 #include <stdint.h>
-#include <fcntl.h>
-#include <errno.h>
-#include <termios.h>
-#include <unistd.h>
-#include <array>
 #include <string>
-#include "rclcpp/rclcpp.hpp"
+#include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 
-#define NO_ERROR            0
-#define ERR_SERIAL_FAILURE  1
-#define ERR_SLAVE_INVALID   2
-#define ERR_NO_RESPONSE     3
-#define ERR_FRAME_CORRUPTED 4
+enum class AbsencErrorCause : uint8_t {
+    NONE,
+    SERIAL_FAILURE,
+    SLAVE_INVALID,
+    NO_RESPONSE,
+    FRAME_CORRUPTED,
+};
 
-typedef struct {
-    int error;
+const char* to_string(AbsencErrorCause cause);
+
+struct AbsencError {
+    AbsencErrorCause error;
     int cause;
     int line;
-} ABSENC_Error_t;
+};
 
-const char* strAbsencErr(int err);
-
-
-typedef struct {
+struct AbsencMeasurement {
     uint8_t slvnum;
     uint16_t status;
     double angval;
     double angspd;
-} ABSENC_Meas_t;
+};
 
-#define no_error (ABSENC_Error_t{0, 0, __LINE__})
+static constexpr auto NO_ERROR = AbsencError{AbsencErrorCause::NONE, 0, __LINE__};
 
 class AbsencDriver {
 public:
-    static ABSENC_Error_t OpenPort(const char* path, int& s_fd);
-    static ABSENC_Error_t PollSlave(int slvnum, ABSENC_Meas_t* meas, int s_fd);
-    static ABSENC_Error_t ClosePort(int s_fd);
+    static AbsencError openPort(const char* path, int& s_fd);
+    static AbsencError pollSlave(int slvnum, AbsencMeasurement* meas, int s_fd);
+    static AbsencError closePort(int s_fd);
 };
 
 class Absenc : public rclcpp::Node {
 public:
     Absenc();
-    ~Absenc();
+    ~Absenc() override;
 
 private:
-
     float old_angle_4 = 0;
 
     int8_t angle_4_zone = 0;
