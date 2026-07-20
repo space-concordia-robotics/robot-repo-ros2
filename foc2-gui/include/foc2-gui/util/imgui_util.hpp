2a954a61899f4ceac985383c6845cd85c929e1a1
Will <william.free@12oclockpoint.com>
2026-07-20 00:09:11 -0400

    Fix compilation issues in modern fmt versions
    Signed-off-by: Will <william.free@12oclockpoint.com>

--- foc2-gui/include/foc2-gui/util/imgui_util.hpp
+++ foc2-gui/include/foc2-gui/util/imgui_util.hpp
@@ -2,7 +2,7 @@
 
 #include <imgui.h>
 #include <string>
-#include <fmt/core.h>
+#include <fmt/format.h>
 
 namespace ImGui {
     constexpr ImU32 ImColor(const uint8_t red, const uint8_t green, const uint8_t blue, const uint8_t alpha) noexcept {
